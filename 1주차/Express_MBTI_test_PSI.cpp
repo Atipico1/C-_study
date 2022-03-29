b62a3e0ac291fae283f0a884aa3e5822b0e79e29
Atipico1 <100078632+Atipico1@users.noreply.github.com>
2022-03-30 01:49:52 +0900

    Update Express_MBTI_test_PSI.cpp

--- 1주차/Express_MBTI_test_PSI.cpp
+++ 1주차/Express_MBTI_test_PSI.cpp
@@ -12,33 +12,30 @@ int main(void) {
     while (start != "go");
 
     cout << "문제가 생겼을 때 당신은 말이 많아지는 편(Y)인가요 생각이 많아지는 편인가요(N)? " << endl;
-    string answer1;
-    cin >> answer1;
-    if (answer1 == "Y" || answer1 == "y")
+    string answer;
+    cin >> answer;
+    if (answer == "Y" || answer == "y")
     result += "E";
     else
     result += "I";
 
     cout << "문제가 생겼을 때 당신은 '그냥 그런가 보다'라고 생각하는 편인가요(Y) '어떻게 그럴 수 있지'라고 생각하는 편인가요(N)? "<< endl;
-    string answer2;
-    cin >> answer2;
-    if (answer2 == "Y" || answer2 == "y")
+    cin >> answer;
+    if (answer == "Y" || answer == "y")
     result += "S";
     else
     result += "N";
 
     cout << "당신은 이해가 안되도 공감을 할 수 있는 편인가요(Y) 이해가 돼야 공감을 하는 편인가요(N)? "<< endl;
-    string answer3;
-    cin >> answer3;
-    if (answer3 == "Y" || answer3 == "y")
+    cin >> answer;
+    if (answer == "Y" || answer == "y")
     result += "F";
     else
     result += "T";
 
     cout << "당신은 한다면 하는 편(Y)인가요 뭐부터 할지 고민하는 편인가요(N)? "<< endl;
-    string answer4;
-    cin >> answer4;
-    if (answer4 == "Y" || answer4 == "y")
+    cin >> answer;
+    if (answer == "Y" || answer == "y")
     result += "J";
     else
     result += "P";
