1c11d2ef4f9b8fe3a8a57697b38d84518054a351
jessy-kim <68943859+jessy-kim@users.noreply.github.com>
2022-04-01 14:55:36 +0900

    Add files via upload

---
+++ 1주차/loving condition.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//mbti enum
+enum mbti{
+    ENFP=1,
+    ENFJ=2,
+    ENTP=3,
+    ENTJ=4,
+    ESFP=5,
+    ESTP=6,
+    ESFJ=7,
+    ESTJ=8,
+    INFP=9,
+    INFJ=10,
+    INTP=11,
+    INTJ=12,
+    ISFP=13,
+    ISTP=14,
+    ISFJ=15,
+    ISTJ=16
+};
+
+int loving_mbti;
+
+void final_selection(int loving){
+    switch (loving)
+    {
+        case ENFP:
+            std::cout<<"ENFP";
+            break;
+        case ENFJ:
+            std::cout<<"ENFJ";
+            break;
+        case ENTP:
+            std::cout<<"ENTP";
+            break;
+        case ENTJ:
+            std::cout<<"ENTJ";
+            break;
+        case ESFP:
+            std::cout<<"ESFP";
+            break;
+        case ESTP:
+            std::cout<<"ESTP";
+            break;
+        case ESFJ:
+            std::cout<<"ESFJ";
+            break;
+        case ESTJ:
+            std::cout<<"ESTJ";
+            break;
+        case INFP:
+            std::cout<<"INFP";
+            break;
+        case INFJ:
+            std::cout<<"INFJ";
+            break;
+        case INTP:
+            std::cout<<"INTP";
+            break;
+        case INTJ:
+            std::cout<<"INTJ";
+            break;
+        case ISFP:
+            std::cout<<"ISFP";
+            break;
+        case ISTP:
+            std::cout<<"ISTP";
+            break;
+        case ISFJ:
+            std::cout<<"ISFJ";
+            break;
+        case ISTJ:
+            std::cout<<"ISTJ";
+            break;
+    }
+}
+
+int main(){
+    do {
+    std::cout<<"Choose MBTI that attracts you the most! \n";
+    std::cout<<"ENFP=1,ENFJ=2,ENTP=3,ENTJ=4,ESFP=5,ESTP=6,ESFJ=7,ESTJ=8,INFP=9,INFJ=10,INTP=11,INTJ=12,ISFP=13,ISTP=14,ISFJ=15,ISTJ=16 \n";
+    std::cout<<": ";
+    std::cin>>loving_mbti;
+    }
+while(
+    loving_mbti != 1 && loving_mbti != 2 && loving_mbti != 3 && 
+    loving_mbti != 4 && loving_mbti != 5 && loving_mbti != 6 &&
+    loving_mbti != 7 && loving_mbti != 7 && loving_mbti != 8 &&
+    loving_mbti != 9 && loving_mbti != 10 &&
+    loving_mbti != 11 && loving_mbti != 12 && loving_mbti != 13 && 
+    loving_mbti != 14 && loving_mbti != 15 && loving_mbti != 16
+);
+
+final_selection(loving_mbti);
+}
